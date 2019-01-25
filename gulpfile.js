9069f59791aee48760364de3bc12ef98edd6c63f
kevin leòn <kroa@quadiontech.com>
2019-01-25 12:22:55 -0300

    bootstrap 4.2.1 and lint installed and configured

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var open = require('gulp-open'); //Open a URL in a web browser
 var browserify = require('browserify'); // Bundles JS
 var reactify = require('reactify');  // Transforms React JSX to JS
 var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+var concat = require('gulp-concat'); //Concatenates files
+var lint = require('gulp-eslint'); //Lint JS files, including JSX
+
 
 var config= {
 	port:9005,
@@ -13,6 +16,10 @@ var config= {
 	paths: {
 		html: "./src/*.html",
 		js: './src/**/*.js',
+		css: [
+      		'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    		],
 		dist: "./dist",
 		mainJs: './src/main.js'
 
@@ -27,6 +34,12 @@ gulp.task("connect", function(){
 		base: config.devBaseUrl,
 		livereload: true
 	});
+});
+
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
 gulp.task("open", ["connect"], function(){
@@ -50,9 +63,18 @@ gulp.task('js', function() {
 		.pipe(connect.reload());
 });
 
-gulp.task("watch", function(){
-	gulp.watch(config.paths.html , ['html']);
-	gulp.watch(config.paths.js, ['js']);
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({config: 'eslint.config.json'}))
+		.pipe(lint.format());
 });
 
-gulp.task('default', ["html", 'js', "open", "watch"]);
+gulp.task("watch", function(){
+	gulp.watch(config.paths.html , ['html']);
+	gulp.watch(config.paths.js, ["js", "lint"]);
+});
+
+
+
+
+gulp.task('default', ["html", "js", "css", "open", "watch", "lint"]);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery = require('jquery');
+
 var App= console.log("hellow world from browserify");
 
 module.exports= App;
